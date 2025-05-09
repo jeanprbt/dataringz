98861c3a7bd3906e1b3a84d67b13970763bca0f0
Jean Perbet <jeanperbet@icloud.com>
2025-05-09 14:31:07 +0200

    feat: adjust navigation buttons & page modal style

--- components/PageModal.vue
+++ components/PageModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="show" :class="['fixed h-full w-full flex items-center justify-center z-10', modalBackgroundClass]"
         @click="close">
-        <div :class="['relative rounded-xl shadow-lg flex flex-col max-w-7xl p-1 w-[95%] md:w-[90%] h-[95%] md:h-[90%] overflow-hidden', modalShapeClass]"
+        <div :class="['relative rounded-xl flex flex-col max-w-7xl p-1 w-[95%] md:w-[90%] h-[95%] md:h-[90%] overflow-hidden', modalShapeClass]"
             @click.stop>
             <div :class="['sticky top-0 z-10 flex items-center py-4 px-4 opacity-0 justify-between', modalHeaderClass]">
                 <button v-if="back && isSmallScreen" @click="emit('back')"
@@ -30,7 +30,7 @@
             <svg class="absolute top-0 left-0 w-full h-full pointer-events-none" xmlns="http://www.w3.org/2000/svg">
                 <rect width="100%" height="100%" fill="none" stroke-width="6" stroke-dasharray="400%" rx="12" ry="12"
                     stroke-dashoffset="400%"
-                    :class="['stroke-white/80 dark:stroke-zinc-700/80', modalRectangleClass]" />
+                    :class="['stroke-white dark:stroke-zinc-700', modalRectangleClass]" />
             </svg>
         </div>
     </div>
@@ -67,7 +67,7 @@ const items = toRef(props, "items");
 // UI STATE
 const exit = ref(false);
 const modalBackgroundClass = computed(() => exit.value ? props.countries ? 'animate-modal-background-countries-exit' : 'animate-modal-background-exit' : props.transition ? 'bg-black/70' : props.countries ? 'animate-modal-background dark:animate-modal-background-countries' : 'animate-modal-background');
-const modalShapeClass = computed(() => exit.value ? 'animate-modal-shape-light-exit dark:animate-modal-shape-dark-exit' : props.transition ? 'bg-white/90 dark:bg-zinc-800/90' : 'animate-modal-shape-light dark:animate-modal-shape-dark');
+const modalShapeClass = computed(() => exit.value ? 'animate-modal-shape-light-exit dark:animate-modal-shape-dark-exit' : props.transition ? 'bg-white dark:bg-zinc-800' : 'animate-modal-shape-light dark:animate-modal-shape-dark');
 const modalContentClass = computed(() => exit.value ? 'animate-modal-content-exit' : props.transition ? 'animate-modal-content-transition' : 'animate-modal-content');
 const modalHeaderClass = computed(() => exit.value ? 'animate-modal-content-exit' : props.transition ? 'opacity-100' : 'animate-modal-content');
 const modalContentContainerClass = computed(() => exit.value ? 'opacity-0' : 'opacity-100');
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="absolute flex top-5 left-1/2 transform -translate-x-1/2">
-        <UTooltip :delay-duration="0.2" arrow :content="{ side: 'bottom', sideOffset: 0 }" text="Olympics Overview">
-            <button ref="olympicsButton" @click="router.push('/olympics')" v-show="showOlympicsButton"
-                class="flex items-center justify-center mr-2 px-1 rounded-lg shadow-sm backdrop-blur-xl border-1 border-zinc-300 hover:border-zinc-200 dark:border-zinc-600 hover:dark:border-zinc-700 w-[50px] h-[40px] bg-white/30 dark:bg-zinc-900/30">
-                <img src="/img/olympics.svg" alt="olympics logo" class="size-9" />
-            </button>
-        </UTooltip>
+        <!-- <UTooltip :delay-duration="0.2" arrow :content="{ side: 'bottom', sideOffset: 0 }" text="Olympics Overview"> -->
+        <button ref="olympicsButton" @click="router.push('/olympics')" v-show="showOlympicsButton"
+            class="flex items-center justify-center mr-2 px-1 rounded-lg shadow-sm border-1 border-zinc-300 hover:border-zinc-500 dark:border-zinc-600 hover:dark:border-zinc-400 w-[50px] h-[40px] bg-white dark:bg-zinc-900">
+            <img src="/img/olympics.svg" alt="olympics logo" class="size-9" />
+        </button>
+        <!-- </UTooltip> -->
 
         <button ref="searchButton" @click="searchButtonClicked" v-show="showSearchButton"
-            class="flex items-center text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 hover:dark:text-zinc-300 px-4 py-2 rounded-lg shadow-sm backdrop-blur-xl border-1 border-zinc-300 hover:border-zinc-200 dark:border-zinc-600 hover:dark:border-zinc-700 whitespace-nowrap bg-white/30 dark:bg-zinc-900/30">
+            class="flex items-center text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 hover:dark:text-zinc-300 px-4 py-2 rounded-lg shadow-sm border-1 border-zinc-300 hover:border-zinc-500 dark:border-zinc-600 hover:dark:border-zinc-400 whitespace-nowrap bg-white dark:bg-zinc-900">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 mr-2">
                 <path fill-rule="evenodd"
                     d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
@@ -21,13 +21,12 @@
             </UKbd>
         </button>
 
-        <UTooltip :delay-duration="0" arrow :content="{ side: 'bottom', sideOffset: 0 }"
-            :text="section === 'map' ? 'See countries stats' : 'Go back to Paris'">
-            <button ref="globeButton" @click="globeButtonClicked" v-show="showGlobeButton"
-                class="flex items-center justify-center text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 hover:dark:text-zinc-300 ml-2 px-[9px] rounded-lg shadow-sm backdrop-blur-2xl border-1 border-zinc-300 hover:border-zinc-200 dark:border-zinc-600 hover:dark:border-zinc-700 bg-white/30 dark:bg-zinc-900/30">
-                <UIcon :name="globeIcon" class="size-6" />
-            </button>
-        </UTooltip>
+        <!-- <UTooltip :delay-duration="0" arrow :content="{ side: 'bottom', sideOffset: 0 }" :text="section === 'map' ? 'See countries stats' : 'Go back to Paris'"> -->
+        <button ref="globeButton" @click="globeButtonClicked" v-show="showGlobeButton"
+            class="flex items-center justify-center text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 hover:dark:text-zinc-300 ml-2 px-[9px] rounded-lg shadow-sm border-1 border-zinc-300 hover:border-zinc-500 dark:border-zinc-600 hover:dark:border-zinc-400 bg-white dark:bg-zinc-900">
+            <UIcon :name="globeIcon" class="size-6" />
+        </button>
+        <!-- </UTooltip> -->
     </div>
     <UModal v-model:open="open" class="w-[80%] md:w-[35%] h-auto bg-opacity-0 backdrop-blur-3xl rounded-xl"
         :overlay="false" :ui="{ content: 'ring-zinc-300 dark:ring-zinc-600' }">
